af1011c416a9531654a718c0946ed6620c8ac93b
Tom Shiflet <tom.shiflet@mylcs.com>
2019-08-30 15:25:43 -0500

    TJS

--- NumGuesser/main.swift
+++ NumGuesser/main.swift
@@ -8,5 +8,44 @@
 
 import Foundation
 
-print("Hello, World!")
+var quitGame:Bool=false
 
+while !quitGame
+{
+    var roundOver:Bool=false
+    var high:Int=100
+    var low:Int=1
+    var guess:Int=0
+    
+    
+    while !roundOver
+    {
+        guess=(high+low)/2
+        print("My guess is: \(guess)")
+        print("Is this too [h]igh, too [l]ow, or [c]orrect?")
+        let input=readLine()!
+        switch input
+        {
+            case "h":
+                
+                high=guess
+            case "l":
+                low=guess
+            
+            case "c":
+                print("Hurray! I got it!")
+                roundOver=true
+            default:
+                print("Invalid input")
+            
+        } // switch input
+
+    } // while the round isn't over
+    print("Do you want to play again? [y]/[n]")
+    let playAgain=readLine()!
+    if playAgain=="n"
+    {
+        quitGame=true
+    } // if
+    
+} // while the game is running
