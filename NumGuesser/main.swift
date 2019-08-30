641e4c0afdc2a0f4c74d04fd395e3b8f4244805a
Tom Shiflet <tom.shiflet@mylcs.com>
2019-08-30 14:40:34 -0500

    Initial Commit

---
+++ NumGuesser/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  NumGuesser
+//
+//  Created by Tom Shiflet on 8/30/19.
+//  Copyright © 2019 Tom Shiflet. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
